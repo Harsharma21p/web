71e912de0a1f75c177a3dd5fe92a184a2c6163c7
Harsharma21 <you@example.com>
2024-01-18 18:27:08 +0530

    hllo

---
+++ harsh/style.js
@@ -0,0 +1,20 @@
+// let obj={
+//     id:1,
+//     user:"hello",
+//     print:function(){
+//     console.log(this.user)
+
+//     }
+
+// }
+// obj.print()
+
+// console.log(obj.toString())
+
+
+
+let arr=[1,2,3,4,5,6]
+
+arr.map((a)=>{
+    console.log(a)
+})
